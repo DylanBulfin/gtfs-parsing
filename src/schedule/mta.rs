7ec6eb14a88a247e1af9265d803bbbfbde9fd40d
Dylan Bulfin <dylanbulfin@gmail.com>
2025-03-27 10:25:17 -0400

    Finish basic parsing for MTA

--- src/schedule/mta.rs
+++ src/schedule/mta.rs
@@ -39,20 +39,7 @@ impl TryFrom<Agency> for MTAgency {
     }
 }
 
-impl MTAgency {
-    fn try_create(value: Agency) -> Result<Self, String> {
-        Ok(Self {
-            agency_id: value.agency_id.ok_or("agency_id cannot be empty")?,
-            agency_name: value.agency_name,
-            agency_url: value.agency_url,
-            agency_timezone: value.agency_timezone,
-            agency_lang: value.agency_lang.ok_or("agency_lang cannot be empty")?,
-            agency_phone: value.agency_phone.ok_or("agency_phone cannot be empty")?,
-        })
-    }
-}
-
-pub struct SubwayRoute<'sc> {
+pub struct SubwayRoute {
     pub route_id: String,
     pub agency_id: String,
     pub route_short_name: String,
@@ -60,14 +47,12 @@ pub struct SubwayRoute<'sc> {
     pub route_desc: String,
     pub route_type: RouteType,
     pub route_url: String,
-    pub route_color: String,
-    pub route_text_color: String,
-
-    pub agency: &'sc MTAgency,
+    pub route_color: Option<String>,
+    pub route_text_color: Option<String>,
 }
 
-impl<'sc> SubwayRoute<'sc> {
-    fn try_create(value: Route, agency: &'sc MTAgency) -> Result<Self, String> {
+impl SubwayRoute {
+    fn try_create(value: Route) -> Result<Self, String> {
         Ok(Self {
             route_id: value.route_id,
             agency_id: value.agency_id.ok_or("agency_id cannot be empty")?,
@@ -80,68 +65,48 @@ impl<'sc> SubwayRoute<'sc> {
             route_desc: value.route_desc.ok_or("route_desc cannot be empty")?,
             route_type: value.route_type,
             route_url: value.route_url.ok_or("route_url cannot be empty")?,
-            route_color: value.route_color.ok_or("route_color cannot be empty")?,
-            route_text_color: value
-                .route_text_color
-                .ok_or("route_text_color cannot be empty")?,
-
-            agency,
+            route_color: value.route_color,
+            route_text_color: value.route_text_color,
         })
     }
 }
 
-pub struct SubwayTrip<'sc> {
+pub struct SubwayTrip {
     pub trip_id: String,
-    //pub route_id: String,
-    //pub service_id: String,
+    pub route_id: String,
+    pub service_id: String,
     pub trip_headsign: String,
     pub direction_id: DirectionType,
-    //pub shape_id: String,
-    pub shape: &'sc SubwayShape,
-    pub service: &'sc SubwayService,
-    pub route: &'sc SubwayRoute<'sc>,
+    pub shape_id: Option<String>,
 }
 
-impl<'sc> SubwayTrip<'sc> {
-    fn try_create(
-        value: Trip,
-        shape: &'sc SubwayShape,
-        service: &'sc SubwayService,
-        route: &'sc SubwayRoute,
-    ) -> Result<Self, String> {
+impl SubwayTrip {
+    fn try_create(value: Trip) -> Result<Self, String> {
         Ok(Self {
             trip_id: value.trip_id,
             trip_headsign: value.trip_headsign.ok_or("trip_headsign cannot be empty")?,
             direction_id: value.direction_id.ok_or("direction_type cannot be empty")?,
-
-            shape,
-            service,
-            route,
+            route_id: value.route_id,
+            service_id: value.service_id,
+            shape_id: value.shape_id,
         })
     }
 }
 
-pub struct SubwayTransferRule<'sc> {
-    //pub from_stop_id: String,
-    //pub to_stop_id: String,
+pub struct SubwayTransferRule {
+    pub from_stop_id: String,
+    pub to_stop_id: String,
     pub min_transfer_time: u32,
-
-    pub from_station: &'sc SubwayStation,
-    pub to_station: &'sc SubwayStation,
 }
 
-impl<'sc> SubwayTransferRule<'sc> {
-    fn try_create(
-        value: Transfer,
-        from_station: &'sc SubwayStation,
-        to_station: &'sc SubwayStation,
-    ) -> Result<Self, String> {
+impl SubwayTransferRule {
+    fn try_create(value: Transfer) -> Result<Self, String> {
         Ok(Self {
             min_transfer_time: value
                 .min_transfer_time
                 .ok_or("min_transfer_time cannot be empty")?,
-            from_station,
-            to_station,
+            from_stop_id: value.from_stop_id.ok_or("from_stop_id cannot be empty")?,
+            to_stop_id: value.to_stop_id.ok_or("to_stop_id cannot be empty")?,
         })
     }
 }
@@ -153,7 +118,7 @@ pub struct SubwayStation {
     pub stop_lon: String,
 
     pub uptown_platform_id: String,
-    pub downtown_platform: String,
+    pub downtown_platform_id: String,
 }
 
 impl SubwayStation {
@@ -173,41 +138,34 @@ impl SubwayStation {
             stop_lon: stop_lon.ok_or("stop_lon cannot be empty")?,
 
             uptown_platform_id: uptown.stop_id,
-            downtown_platform: downtown.stop_id,
+            downtown_platform_id: downtown.stop_id,
         })
     }
 }
 
-pub struct SubwayStopTime<'sc> {
-    //pub trip_id: String,
+pub struct SubwayStopTime {
+    pub trip_id: String,
     pub arrival_time: String,
     pub departure_time: String,
-    //pub stop_id: String,
+    pub station_id: String,
     pub stop_sequence: u32,
 
-    pub trip: &'sc SubwayTrip<'sc>,
-    pub station: &'sc SubwayStation,
     pub uptown: bool, // For each station the MTA format defines a parent station, and two
                       // platforms (N and S). This determines which platform this stoptime
                       // uses
 }
 
-impl<'sc> SubwayStopTime<'sc> {
-    fn try_create(
-        value: StopTime,
-        trip: &'sc SubwayTrip<'sc>,
-        station: &'sc SubwayStation,
-        uptown: bool,
-    ) -> Result<Self, String> {
+impl SubwayStopTime {
+    fn try_create(value: StopTime, station_id: String, uptown: bool) -> Result<Self, String> {
         Ok(Self {
+            trip_id: value.trip_id,
+            station_id,
             arrival_time: value.arrival_time.ok_or("arrival_time cannot be empty")?,
             departure_time: value
                 .departure_time
                 .ok_or("departure_time cannot be empty")?,
             stop_sequence: value.stop_sequence.ok_or("stop_sequence cannot be empty")?,
 
-            trip,
-            station,
             uptown,
         })
     }
@@ -218,19 +176,19 @@ use Service as SubwayService;
 use ServiceException as SubwayServiceException;
 use Shape as SubwayShape;
 
-pub struct SubwaySchedule<'sc> {
+pub struct SubwaySchedule {
     agency: MTAgency,
-    routes: Vec<SubwayRoute<'sc>>,
-    trips: Vec<SubwayTrip<'sc>>,
-    transfers: Vec<SubwayTransferRule<'sc>>,
+    routes: Vec<SubwayRoute>,
+    trips: Vec<SubwayTrip>,
+    transfers: Vec<SubwayTransferRule>,
     stations: Vec<SubwayStation>,
-    stop_times: Vec<SubwayStopTime<'sc>>,
+    stop_times: Vec<SubwayStopTime>,
     services: Vec<SubwayService>,
     service_exceptions: Vec<SubwayServiceException>,
     shapes: Vec<SubwayShape>,
 }
 
-impl<'sc> TryFrom<Schedule> for SubwaySchedule<'sc> {
+impl TryFrom<Schedule> for SubwaySchedule {
     type Error = String;
 
     fn try_from(value: Schedule) -> Result<Self, Self::Error> {
@@ -247,7 +205,7 @@ impl<'sc> TryFrom<Schedule> for SubwaySchedule<'sc> {
         } = value;
 
         assert_eq!(agencies.len(), 1);
-        let agency = MTAgency::try_create(agencies.pop().unwrap())?;
+        let agency = MTAgency::try_from(agencies.pop().unwrap())?;
 
         let mut schedule = Self {
             agency,
@@ -263,32 +221,11 @@ impl<'sc> TryFrom<Schedule> for SubwaySchedule<'sc> {
 
         //let mut routes: Vec<SubwayRoute> = Vec::new();
         for route in base_routes {
-            schedule
-                .routes
-                .push(SubwayRoute::try_create(route, &schedule.agency)?);
+            schedule.routes.push(SubwayRoute::try_create(route)?);
         }
 
         for trip in base_trips {
-            let shape_id = trip.shape_id.as_ref().ok_or("shape_id cannot be empty")?;
-            let shape = shapes
-                .iter()
-                .find(|s| &s.shape_id == shape_id)
-                .ok_or("Invalid shape_id")?;
-
-            let service = services
-                .iter()
-                .find(|s| s.service_id == trip.service_id)
-                .ok_or("Invalid service_id")?;
-
-            let route = schedule
-                .routes
-                .iter()
-                .find(|r| r.route_id == trip.route_id)
-                .ok_or("Invalid route_id")?;
-
-            schedule
-                .trips
-                .push(SubwayTrip::try_create(trip, shape, service, route)?);
+            schedule.trips.push(SubwayTrip::try_create(trip)?);
         }
 
         assert_eq!(base_stops.len() % 3, 0);
@@ -307,10 +244,7 @@ impl<'sc> TryFrom<Schedule> for SubwaySchedule<'sc> {
         assert_eq!(base_stops.len(), 0);
 
         for stop_time in base_stop_times {
-            let platform_id = stop_time
-                .stop_id
-                .as_ref()
-                .ok_or("stop_id cannot be empty")?;
+            let platform_id = stop_time.stop_id.clone().ok_or("stop_id cannot be empty")?;
             assert!(platform_id.is_ascii());
 
             let uptown = match platform_id.as_bytes()[platform_id.len() - 1] {
@@ -319,51 +253,56 @@ impl<'sc> TryFrom<Schedule> for SubwaySchedule<'sc> {
                 c => panic!("Unexpected character at end of stop: {}", c),
             };
 
-            let station = schedule
-                .stations
-                .iter()
-                .find(|s| s.station_id == platform_id[0..platform_id.len() - 1])
-                .ok_or("Invalid stop_id")?;
-
-            let trip = schedule
-                .trips
-                .iter()
-                .find(|t| t.trip_id == stop_time.trip_id)
-                .ok_or("Invalid trip_id in StopTime")?;
-
             schedule.stop_times.push(SubwayStopTime::try_create(
-                stop_time, trip, station, uptown,
+                stop_time,
+                platform_id[0..platform_id.len() - 1].to_owned(),
+                uptown,
             )?)
         }
 
         for transfer in base_transfers {
-            let from_station_id = transfer
-                .from_stop_id
-                .as_ref()
-                .ok_or("from_stop_id cannot be empty")?;
-            let to_station_id = transfer
-                .to_stop_id
-                .as_ref()
-                .ok_or("to_stop_id cannot be empty")?;
-
-            let from_station = schedule
-                .stations
-                .iter()
-                .find(|s| &s.station_id == from_station_id)
-                .ok_or("Invalid from_station_id")?;
-            let to_station = schedule
-                .stations
-                .iter()
-                .find(|s| &s.station_id == to_station_id)
-                .ok_or("Invalid to_station_id")?;
-
-            schedule.transfers.push(SubwayTransferRule::try_create(
-                transfer,
-                from_station,
-                to_station,
-            )?);
+            schedule
+                .transfers
+                .push(SubwayTransferRule::try_create(transfer)?);
         }
 
         Ok(schedule)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::assert_matches::assert_matches;
+
+    use crate::schedule::Schedule;
+
+    use super::SubwaySchedule;
+
+    #[test]
+    fn test_basics() -> Result<(), String> {
+        let schedule = Schedule::from_dir("./test_data");
+
+        let mta_schedule = SubwaySchedule::try_from(schedule)?;
+
+        // Verify Agency
+        assert_eq!(mta_schedule.agency.agency_id, "MTA NYCT");
+        assert_eq!(mta_schedule.agency.agency_name, "MTA New York City Transit");
+        assert_eq!(mta_schedule.agency.agency_url, "http://www.mta.info");
+        assert_eq!(mta_schedule.agency.agency_timezone, "America/New_York");
+        assert_eq!(mta_schedule.agency.agency_lang, "en");
+        assert_eq!(mta_schedule.agency.agency_phone, "718-330-1234");
+
+        // Verify collection lengths
+        assert_eq!(mta_schedule.routes.len(), 30);
+        assert_eq!(mta_schedule.trips.len(), 20298);
+        assert_eq!(mta_schedule.stations.len(), 499);
+        assert_eq!(mta_schedule.stop_times.len(), 10000);
+        assert_eq!(mta_schedule.transfers.len(), 616);
+
+        for route in mta_schedule.routes {
+            assert_eq!(route.agency_id, "MTA NYCT");
+        }
+
+        Ok(())
     }
 }
